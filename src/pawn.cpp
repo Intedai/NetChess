4adabc2c766d94733c845010f90cfc6011750b68
Intedai <69306633+Intedai@users.noreply.github.com>
2026-05-05 02:48:30 +0300

    finished pawn validation (without en passant)

--- src/pawn.cpp
+++ src/pawn.cpp
@@ -14,8 +14,39 @@ bool Pawn::isValidMove(Board& board, int fromRow, int fromCol, int toRow, int to
 
     std::optional<Color> toColor = board.getPieceColor(toRow, toCol);
     
-    // Check if either going forward once (or twice in first time, which i can check because it's a trackedpiece)
-    // or if eating, and if not return false
+    int rowDiff = toRow - fromRow;
+    int colDiff = toCol - fromCol;
+    
+    if (rowDiff == 0)
+        return false;
+    
+    int rowDist = abs(rowDiff);
+    int colDist = abs(colDiff);
 
-    return true;
+    int rowDirection = rowDiff / rowDist;
+    
+    if (rowDirection != direction)
+        return false;
+    
+    // Moves straight 1 or 2 squares vertically without capturing 
+    if (
+        !toColor.has_value() &&
+        colDist == 0 &&
+        (rowDist == 1 || (
+            !moved &&
+            rowDist == 2 &&
+            // Make sure it doesn't pass through a piece
+            !board.getPieceColor(fromRow + direction, toCol).has_value()
+        ))
+    )
+        return true;
+    // Capturing
+    else if (
+        toColor.has_value() &&
+        rowDist == 1 &&
+        colDist == 1 &&
+        color != *toColor
+    )
+        return true;
+    return false;
 }
